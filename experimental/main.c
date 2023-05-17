16e9374509af25d80c1897baa2387e3c171d3663
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-05-17 15:40:17 -0600

    refactor code

--- experimental/main.c
+++ experimental/main.c
@@ -6,46 +6,49 @@
 #include <string.h>
 
 // Constant variables
-static const char deviceName[256] = "E36103B";
+static const char* deviceName = "E36103B";
 static libusb_device_handle *primaryDeviceHandle;
 static libusb_device *primaryDevice;
 
 // Prints the port each device is plugged into
-int list_devices() {
-    // This will store the list of devices. It is allovated by libusb
-    libusb_device **devices;
+int list_devices()
+{
+	// This will store the list of devices. It is allovated by libusb
+	libusb_device **devices;
 
-    // The first argument could be a context, if we cared about not sharing sessions.
-    ssize_t count = libusb_get_device_list(NULL, &devices);
+	// The first argument could be a context, if we cared about not sharing sessions.
+	ssize_t count = libusb_get_device_list(NULL, &devices);
 
-    // Normally, libusb_get_device_list returns a nonnegative number. If it's negative, there was an error.
-    if (count < 0) {
-        printf("Error listing devices: %f\n", count);
-        return 1;
-    }
+	// Normally, libusb_get_device_list returns a nonnegative number. If it's negative, there was an error.
+	if (count < 0)
+	{
+		printf("Error listing devices: %f\n", count);
+		return 1;
+	}
 
-    // For each device, print the port number.
-    for (ssize_t i = 0; i < count; i++) {
-        printf("Device connected on port %d\n", libusb_get_port_number(devices[i]));
-        struct libusb_device_descriptor desc;
-        int desc_code = libusb_get_device_descriptor(devices[i], &desc);
-        if (desc_code != 0)
-        {
+	// For each device, print the port number.
+	for (ssize_t i = 0; i < count; i++)
+	{
+		printf("Device connected on port %d\n", libusb_get_port_number(devices[i]));
+		struct libusb_device_descriptor desc;
+		int desc_code = libusb_get_device_descriptor(devices[i], &desc);
+		if (desc_code != 0)
+		{
 			printf("Error getting device descriptor, error code: %d, error str: %s\n", 
 			desc_code, libusb_error_name(desc_code));
 		}else
 		{
-                        // Print full descriptor (doesn't require comunicating with device)
-                        printf("  Descriptor type: %d\n", desc.bDescriptorType);
-                        printf("  USB version: %d\n", desc.bcdUSB);
-                        printf("  Class: %d\n", desc.bDeviceClass);
-                        printf("  Subclass: %d\n", desc.bDeviceSubClass);
-                        printf("  Protocol: %d\n", desc.bDeviceProtocol);
-                        printf("  Max packet size: %d\n", desc.bMaxPacketSize0);
-                        printf("  Vendor ID: %d\n", desc.idVendor);
-                        printf("  Product ID: %d\n", desc.idProduct);
-                        printf("  Device version: %d\n", desc.bcdDevice);
-                        printf("  Configurations: %d\n", desc.bNumConfigurations);
+			// Print full descriptor (doesn't require comunicating with device)
+			printf("  Descriptor type: %d\n", desc.bDescriptorType);
+			printf("  USB version: %d\n", desc.bcdUSB);
+			printf("  Class: %d\n", desc.bDeviceClass);
+			printf("  Subclass: %d\n", desc.bDeviceSubClass);
+			printf("  Protocol: %d\n", desc.bDeviceProtocol);
+			printf("  Max packet size: %d\n", desc.bMaxPacketSize0);
+			printf("  Vendor ID: %d\n", desc.idVendor);
+			printf("  Product ID: %d\n", desc.idProduct);
+			printf("  Device version: %d\n", desc.bcdDevice);
+			printf("  Configurations: %d\n", desc.bNumConfigurations);
 
 			//open device and obtain a handle
 			struct libusb_device_handle* handle;
@@ -66,72 +69,90 @@ int list_devices() {
 				{
 					printf("Error getting device descriptor string, error code: %d, error str: %s\n", 
 						desc_ascii_code, libusb_error_name(desc_ascii_code));
-                    //close device
-				    libusb_close(handle);
-				}else {
+					//close device
+					libusb_close(handle);
+				}else
+				{
 					printf("Device Product Descriptor: '%s'\n", str);
-                    if(!strcmp(str,deviceName)){
-                        printf("Desired Device Found\n");
-                        primaryDeviceHandle = handle;
-                        primaryDevice = devices[i];
-                    }else{
-                        //close device
-				        libusb_close(handle);
-                    }
+					//strcomp returns zero when the two strings are equal
+					if(strcmp(str,deviceName) == 0)
+					{
+						printf("Desired Device Found\n");
+						primaryDeviceHandle = handle; //save to global variable
+						primaryDevice = devices[i];
+					}else
+					{
+						//close device
+						libusb_close(handle);
+					}
 				}
-				
-				
 			}
 		}
-    }
+		printf("\n");
+	}
 
-    // Deallocate the device list
-    // Also unref the devices in the list
-    libusb_free_device_list(devices, 1);
-    return 0;
+	// Deallocate the device list
+	// Also unref the devices in the list
+	libusb_free_device_list(devices, 1);
+	return 0;
 }
 
 int operate_primary_device() {
-    if(primaryDeviceHandle == NULL){
-        printf("Cannot Transfer, device handler was not initialised\n");
-        return -1;
-    }
-    printf("Attempting Configuration\n");
-    int returned; 
-    returned = libusb_set_configuration(primaryDeviceHandle,0); // This may not need to happen, or if it does return 0 other behavior might need changing
-    printf("Returned value %d\n",returned);
-    printf("Getting config_descriptor\n");
-    struct libusb_config_descriptor *primaryConfig;
-    libusb_get_active_config_descriptor (primaryDevice,&primaryConfig);
-    const struct libusb_interface *interfaces = primaryConfig->interface;
-    printf("%d Interfaces found\n",primaryConfig->bNumInterfaces);
+	if(primaryDeviceHandle == NULL)
+	{
+		printf("Cannot transfer data, device handler was not initialised\n");
+		return -1;
+	}
+	printf("Attempting Configuration\n");
+	int returned = 0; 
+	returned = libusb_set_configuration(primaryDeviceHandle,0); // This may not need to happen, or if it does return 0 other behavior might need changing
+	printf("Returned value %d\n",returned);
+	printf("Getting config_descriptor\n");
+	struct libusb_config_descriptor *primaryConfig;
+	libusb_get_active_config_descriptor (primaryDevice,&primaryConfig);
+	const struct libusb_interface *interfaces = primaryConfig->interface;
+	printf("%d Interfaces found\n",primaryConfig->bNumInterfaces);
 
-    /*
-    printf("Claim Interface\n");
-    returned = libusb_claim_interface(primaryDeviceHandle,0);
-    printf("Returned value %d\n",returned);
-    */
+	/*
+	printf("Claim Interface\n");
+	returned = libusb_claim_interface(primaryDeviceHandle,0);
+	printf("Returned value %d\n",returned);
+	*/
 
-    /*printf("Attempting Transfer of message\n");
-    unsigned char endpoint = 0;
-    unsigned char *data = "*IDN?";
-    int transfered = 0;
-    returned = libusb_control_transfer(primaryDeviceHandle,0,0,0,0,data,sizeof(data),0);
-    printf("Returned value %d\n",returned);
-    printf("Bytes Transfered: %d\n",transfered);
-    libusb_close(primaryDeviceHandle);*/
+	/*printf("Attempting Transfer of message\n");
+	unsigned char endpoint = 0;
+	unsigned char *data = "*IDN?";
+	int transfered = 0;
+	returned = libusb_control_transfer(primaryDeviceHandle,0,0,0,0,data,sizeof(data),0);
+	printf("Returned value %d\n",returned);
+	printf("Bytes Transfered: %d\n",transfered);
+	libusb_close(primaryDeviceHandle);*/
 }
 
 int main() {
-    int error = libusb_init(NULL);
-    if (error == 0) {
-        printf("Initialized successfully\n");
-        list_devices();
-        operate_primary_device();
-        libusb_exit(NULL);
-        return 0;
-    } else {
-        printf("Initialization failed with error code %d", error);
-        return 1;
-    }
+	int error = libusb_init(NULL);
+	if (error == 0)
+	{
+		printf("Initialized successfully\n");
+		int listerr = list_devices();
+		if(listerr != 0)
+		{
+			 libusb_exit(NULL);
+			 return 1;
+		}
+		//attempt to communicate with device
+		int operateerr = operate_primary_device();
+		if(operateerr != 0)
+		{
+			libusb_exit(NULL);
+			 return 1;
+		}
+		//else program finished normally
+		libusb_exit(NULL);
+		return 0;
+	} else
+	{
+		printf("Initialization failed with error code %d", error);
+		return 1;
+	}
 }
