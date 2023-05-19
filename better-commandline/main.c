e9e9b5db9d6eed9e8fd2f32de40b5c5c273e9d36
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-05-19 12:56:20 -0600

    add testing to main.c

--- better-commandline/main.c
+++ better-commandline/main.c
@@ -13,5 +13,16 @@ int main(int argc, char** argv)
 {
 	struct arg_info args;
 	int argproc_code = process_args(argc, argv, &args);
+	
+	if(argproc_code == ARGPROC_ERROR) {return -1; }
+	
+	printf("Connecting to device with vendor id: %d and product id: %d\n",
+		args.vendor_id, args.product_id);
+	printf("Command: %s\n", args.message);
+	if(args.needs_response)
+	{
+		printf("Awaiting response.\n");
+	}
+	
 	return 0;
 }
