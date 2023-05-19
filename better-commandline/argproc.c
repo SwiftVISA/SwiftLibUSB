d80cc226d8748aef1a1b3c100d7ac3ef91520d02
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-05-19 13:04:53 -0600

    more error checking during argument parsing

--- better-commandline/argproc.c
+++ better-commandline/argproc.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int process_args(int argc, char** argv, struct arg_info* ret)
 {
@@ -11,27 +12,53 @@ int process_args(int argc, char** argv, struct arg_info* ret)
 		return ARGPROC_ERROR;
 	}
 	
+	bool did_v = false;
+	bool did_p = false;
+	bool did_m = false;
+	
 	for(int i = 1; i <= 5; i += 2)
 	{
 		if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "-V") == 0) //vendor id
 		{
 			ret->vendor_id = atoi(argv[i+1]);
+			did_v = true;
 		}else if(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "-P") == 0) //product id
 		{
 			ret->product_id = atoi(argv[i+1]);
+			did_p = true;
 		}else if(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "-C") == 0) //command
 		{
 			ret->needs_response = false;
 			ret->message = argv[i+1];
+			did_m = true;
 		}else if(strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "-Q") == 0) //query
 		{
 			ret->needs_response = true;
 			ret->message = argv[i+1];
+			did_m = true;
 		}else
 		{
 			printf("Argument Error: Unknown argument %s\n", argv[i]);
 			return ARGPROC_ERROR;
 		}
+	}
+	
+	if(!did_v)
+	{
+		printf("Argument Error: no vendor id specified\n");
+		return ARGPROC_ERROR;
+	}
+	
+	if(!did_p)
+	{
+		printf("Argument Error: no product id specified\n");
+		return ARGPROC_ERROR;
+	}
+	
+	if(!did_m)
+	{
+		printf("Argument Error: no command or query given\n");
+		return ARGPROC_ERROR;
 	}
 	
 	return ARGPROC_SUCESS;
