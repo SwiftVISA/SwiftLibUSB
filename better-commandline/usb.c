f77ce67c3e82f02911eeed7604d01e3a28aba59a
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-05-19 15:53:38 -0600

    try to fix linux issue

--- better-commandline/usb.c
+++ better-commandline/usb.c
@@ -110,8 +110,6 @@ int usb_connect(unsigned short vendor_id, unsigned short product_id, struct usb_
                 return -1;
             }
             
-            libusb_set_auto_detach_kernel_driver(usb->handle, 1);
-            
             int configure_code = libusb_set_configuration(usb->handle, 0);
             if (configure_code != 0 && configure_code != -12) { // -12 means the OS configures the device
                 printf("Error configuring device: %d\n", configure_code);
@@ -127,6 +125,9 @@ int usb_connect(unsigned short vendor_id, unsigned short product_id, struct usb_
                 libusb_free_device_list(devices, 1);
                 return -1;
             }
+            
+            libusb_set_auto_detach_kernel_driver(usb->handle, 1);
+            
             int claim_error = libusb_claim_interface(usb->handle, 0);
             if (claim_error != 0) {
                 printf("Error claiming interface: %d\n", claim_error);
