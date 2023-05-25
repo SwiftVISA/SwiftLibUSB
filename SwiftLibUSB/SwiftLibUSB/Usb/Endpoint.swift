3b7f801b0c82c61cd802f86467965f24b8e30bc9
Carole Bouy <bouy.carole@gmail.com>
2023-05-25 14:39:10 -0600

    Add properties of Endpoint

--- SwiftLibUSB/SwiftLibUSB/Usb/Endpoint.swift
+++ SwiftLibUSB/SwiftLibUSB/Usb/Endpoint.swift
@@ -17,4 +17,49 @@ class Endpoint {
     deinit {
         
     }
+    
+    var address: Int {
+        get {
+            Int(descriptor.bEndpointAddress)
+        }
+    }
+    
+    var attributes: Int {
+        get {
+            Int(descriptor.bmAttributes)
+        }
+    }
+    
+    var direction: Direction {
+        get {
+            switch descriptor.bEndpointAddress >> 7 {
+            case 1: return .In
+            case 0: return .Out
+            default: return .Out
+            }
+        }
+    }
+    
+    var transferType: TransferType {
+        get {
+            switch libusb_endpoint_transfer_type(UInt32(descriptor.bmAttributes & 3)) {
+            case LIBUSB_ENDPOINT_TRANSFER_TYPE_BULK: return .bulk
+            case LIBUSB_ENDPOINT_TRANSFER_TYPE_ISOCHRONOUS: return .isochronous
+            case LIBUSB_ENDPOINT_TRANSFER_TYPE_INTERRUPT: return .interrupt
+            default: return .control
+            }
+        }
+    }
+}
+
+enum Direction {
+    case In
+    case Out
+}
+
+enum TransferType {
+    case bulk
+    case isochronous
+    case interrupt
+    case control
 }
