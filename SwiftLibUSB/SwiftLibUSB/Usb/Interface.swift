2c38eab075a52f565e22709690bde4d534e787fb
Carole Bouy <bouy.carole@gmail.com>
2023-05-25 14:55:45 -0600

    Claim and release interface

--- SwiftLibUSB/SwiftLibUSB/Usb/Interface.swift
+++ SwiftLibUSB/SwiftLibUSB/Usb/Interface.swift
@@ -12,10 +12,12 @@ class Interface {
     var claimed = false
     var altSettings: [AltSetting]
     var device: Device
+    var index: Int
     
-    init(pointer : libusb_interface, device: Device) {
+    init(pointer : libusb_interface, device: Device, index: Int) {
         descriptor = pointer
         self.device = device
+        self.index = index
         
         altSettings = []
         for i in 0..<descriptor.num_altsetting {
@@ -24,10 +26,13 @@ class Interface {
     }
     
     deinit {
-        
+        if claimed {
+            libusb_release_interface(device.handle?.handle, Int32(index))
+        }
     }
     
     func claim(){
-       
+        libusb_claim_interface(device.handle?.handle, Int32(index))
+        claimed = true
     }
 }
