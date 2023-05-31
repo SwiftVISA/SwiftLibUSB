1be2050478c616498f195a016d6ec797a91b3a56
John Hiatt <john.william.hiatt@gmail.com>
2023-05-31 16:04:03 -0700

    Add receiveBulkTransfer to Endpoint

--- SwiftLibUSB/SwiftLibUSB/Usb/Endpoint.swift
+++ SwiftLibUSB/SwiftLibUSB/Usb/Endpoint.swift
@@ -78,6 +78,29 @@ class Endpoint {
         }
         return Int(sent)
     }
+    
+    /// Receives a message from a bulk in endpoint
+    ///
+    /// This will only work properly if this endpoint is bulk in (`direction == .in` and `.transferType == .bulk`)
+    ///
+    /// - returns: the number of bytes received
+    /// - throws: a USBError if the transfer fails
+    /// * `.pipe` if the endpoint halts
+    /// * `.noDevice` if the device disconnected
+    /// * `.busy` if libUSB is currently handling events (if you call this from an asynchronous transfer callback, for example)
+    /// * `.invalidParam` if the transfer size is larger than the OS or device support
+    /// * `.overflow` if more data was sent than was requested
+    func receiveBulkTransfer() throws -> Data {
+        libusb_clear_halt(device.handle?.handle, descriptor.bEndpointAddress)
+        var sent: Int32 = 0;
+        var innerData = [UInt8](repeating: 0, count: 1024)
+        let length: Int32 = 1024
+        let error = libusb_bulk_transfer(device.handle?.handle, descriptor.bEndpointAddress, &innerData, length, &sent, 1000)
+        if error < 0 {
+            throw USBError.from(code: error)
+        }
+        return Data(innerData)
+    }
 }
 
 /// Describes the direction of data transfer on an endpoint.
