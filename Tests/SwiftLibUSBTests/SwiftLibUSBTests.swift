99988190666f7225fc4743263bdbcc3044407fa4
BRH0208 <bryce@hawken.net>
2023-06-09 12:51:15 -0600

    added old test cases

--- Tests/SwiftLibUSBTests/SwiftLibUSBTests.swift
+++ Tests/SwiftLibUSBTests/SwiftLibUSBTests.swift
@@ -1,11 +1,39 @@
+//
+//  SwiftLibUSBTests.swift
+//  SwiftLibUSBTests
+//
+//  Created by Carole Bouy (Student) on 5/23/23.
+//
+
 import XCTest
 @testable import SwiftLibUSB
 
 final class SwiftLibUSBTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftLibUSB().text, "Hello, World!")
+    var instrument: USBTMCInstrument?
+
+    override func setUpWithError() throws {
+        try instrument = USBTMCInstrument(visaString: "USB0::10893::5634::MY59001442::0::INSTR")
     }
+
+    override func tearDown() {
+        instrument = nil
+    }
+
+    func testLargeCommand() throws {
+        try instrument?.write(":SOURCE:VOLTAGE MINIMUM; :SOURCE:CURRENT MINIMUM", appending: "\n", encoding: .ascii)
+    }
+    
+    func testRawResponse() throws {
+        try instrument?.write("VOLT 1;VOLT?")
+        let response = try instrument!.readBytes(length: 1024, chunkSize: 16)
+        XCTAssert(response == "+1.000000E+00 \n".data(using: .utf8))
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
